75b700aec0e83e9c52608d107169e798299cf15e
Brady Liechty <berrybloxinator@gmail.com>
2020-02-10 12:16:10 -0700

    category and order are now stored in a cookie instead of query parameter

--- routes/route.js
+++ routes/route.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const app = express.Router();
 const repository = require("../repositories/userRepository");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.get("/userList", (req, res) => {
-    let category = "";
-    let order = "";
-    if (Object.keys(req.query).length !== 0) {
-        category = req.query.category;
-        order = req.query.order;
-    } else {
-        category = "_id";
-        order = "ascending";
-    }
+    const cookies = checkForCategoryAndOrder(req.cookies);
+    const category = cookies.category;
+    const order = cookies.order;
     repository.findAll(category, order).then(users => {
         res.render("usersList", {users, category, order});
     }).catch(e => {
@@ -25,27 +21,35 @@ app.get("/userList", (req, res) => {
 
 app.post("/userList", (req, res) => {
     const searchInput = req.body["search-input"];
+    let category;
+    let order;
     if (searchInput === undefined) {
-        const category = req.body["user-category"];
-        const order = req.body["user-order"];
+        category = req.body["user-category"];
+        order = req.body["user-order"];
+        res.cookie("category", category);
+        res.cookie("order", order);
         repository.findAll(category, order).then(users => {
             res.render("usersList", {users, category, order});
         }).catch(e => {
             console.log(`Error occurred: ${e}`);
         });
     } else {
+        const cookies = checkForCategoryAndOrder(req.cookies);
+        category = cookies.category;
+        order = cookies.order;
         if (!/\\/.test(searchInput) &&
             !/\|/.test(searchInput) &&
             !/\+/.test(searchInput) &&
             !/\?/.test(searchInput) &&
-            !/\./.test(searchInput)) {
+            !/\./.test(searchInput) &&
+            searchInput !== "") {
             repository.search(searchInput).then(users => {
-                res.render("usersList", {users, searched: true});
+                res.render("usersList", {users, category, order, searched: true});
             }).catch(e => {
                 console.log(`Error occurred: ${e}`);
             });
         } else {
-            res.render("usersList", {users: [], searched: true});
+            res.render("usersList", {users: [], category, order, searched: true});
         }
     }
 });
@@ -78,7 +82,7 @@ app.post("/createUser", (req, res) => {
 
 app.get("/deleteUser/:userId", (req, res) => {
     repository.delete(req.params.userId);
-    res.redirect(`/userList?category=${req.query.category}&order=${req.query.order}`);
+    res.redirect("/userList");
 });
 
 app.get("/userList/:userId", (req, res) => {
@@ -110,8 +114,23 @@ app.post("/userList/:userId", (req, res) => {
             req.body["age"],
             req.body["email-address"]
         );
-        res.redirect(`/userList?category=${req.query.category}&order=${req.query.order}`);
+        res.redirect("/userList");
     }
 });
+
+function checkForCategoryAndOrder(cookies) {
+    if (cookies.category === undefined &&
+        cookies.order === undefined) {
+        return {
+            category: "_id",
+            order: "ascending"
+        };
+    } else {
+        return {
+            category: cookies.category,
+            order: cookies.order
+        };
+    }
+}
 
 module.exports = app;
